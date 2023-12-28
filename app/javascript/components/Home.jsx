b29971baef06b33ce5c3dd595faaf795a0bb8517
Andrew Lewis <andrew.lewis@appfolio.com>
2023-12-27 16:10:28 -0800

    Add jpg loader and load image asset in Home component

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Waldo from '../../assets/images/waldo.jpg'
 
 export default function Home() {
     return (
-        <h1>Hello, World!</h1>
+        <div>
+            <h1 style={{display:'flex', justifyContent:'center'}}>Can you find Waldo?</h1>
+            <img src={Waldo} alt="waldo" style={{width:'100%'}}/>
+        </div>
     )
 }
