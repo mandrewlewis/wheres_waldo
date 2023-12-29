d844fceea6cceed33bdba99e85a38160a83c83e5
Andrew Lewis <andrew.lewis@appfolio.com>
2023-12-28 16:13:48 -0800

    Integrate Tailwind

--- Gemfile
+++ Gemfile
@@ -71,3 +71,5 @@ group :test do
   gem "webdrivers"
 
 end
+
+gem "tailwindcss-rails", "~> 2.1"
--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -4,8 +4,8 @@ import Waldo from '../../assets/images/waldo.jpg'
 export default function Home() {
     return (
         <div>
-            <h1 style={{display:'flex', justifyContent:'center'}}>Can you find Waldo?</h1>
-            <img src={Waldo} alt="waldo" style={{width:'100%'}}/>
+            <h1 className={'flex justify-center p-4 text-3xl font-bold'}>Can you find Waldo?</h1>
+            <img src={Waldo} alt="waldo" />
         </div>
     )
 }
