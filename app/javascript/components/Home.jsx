fbf719cb2f9b1455504e6b3db587ff9fee4533e1
Andrew Lewis <andrew.lewis@appfolio.com>
2023-12-28 21:55:55 -0800

    Create home page and route game to /game

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <h1 className={'flex justify-center text-5xl p-8'}>
+            Where's Waldo?
+        </h1>
+    )
+}
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../components/Home'
 import Game from '../components/Game'
 
 export default (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Game />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/game" element={<Game />} />
         </Routes>
     </BrowserRouter>
 );
