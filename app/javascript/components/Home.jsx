29ab93f65e457c827cfe82e21d86011b6c0af7b7
Andrew Lewis <andrew.lewis@appfolio.com>
2023-12-29 22:15:44 -0800

    Fix spelling errors

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -33,9 +33,9 @@ export default function Home() {
                 Where's Waldo?
             </h1>
             <p className={'text-lg text-center'}>
-                It's the game you know and love, so get ready to search for the candy cane striped, British chap hiding
+                It's the game you know and love, so get ready to search for the candy-cane-striped, British chap hiding
                 in the crowd. Simply click on the image to select an area, then click ✅ to confirm or ❌ to cancel
-                your selection. Careful! Wrong guess add 10 seconds to your timer!
+                your selection. Careful! Wrong guesses add 10 seconds to your timer!
             </p>
             <Link to={'/game'}>
                 <button className={'text-xl text-white bg-blue-500 px-2 py-0.5 rounded-md'}>Play</button>
