eb36649a29d0623ef60c01fe155a36646f236513
Andrew Lewis <andrew.lewis@appfolio.com>
2023-12-30 10:06:25 -0800

    Write model tests for Score

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,5 @@
 class Score < ApplicationRecord
+  validates :name, presence: true
+  validates :time, presence: true
   scope :ranked, -> { where(hidden: false).order(:time) }
 end
--- test/models/score_test.rb
+++ test/models/score_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class ScoreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cannot save without name" do
+    score = Score.new(time: 123)
+    assert_not score.save
+  end
+
+  test "cannot save without time" do
+    score = Score.new(name: 'Capy')
+    assert_not score.save
+  end
+
+  test "hidden defaults to false" do
+    score = Score.new(name: 'Capy', time: 123)
+    score.save
+    assert_equal score.hidden, false
+  end
+
+  test "ranked scope orders time asc" do
+    scores = Score.ranked
+    assert scores[0].time <= scores[1].time && scores[1].time <= scores[2].time
+  end
 end
